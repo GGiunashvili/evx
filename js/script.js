d0c43c2fff1865930defe61d390eb8349eaa0724
sal0 <salomemindiashvili5@gmail.com>
2019-08-18 11:46:55 +0400

    update fb chat visibility toggle logic

--- js/script.js
+++ js/script.js
@@ -2,5 +2,6 @@ $(document).ready(function () {
 	$('.burgeer-menu').click(function () {
 		$('.menu').toggleClass('flex');
 		$('.fb-customerchat').toggleClass('hide');
+		$('.fb_dialog').toggleClass('hide');
 	})
 })
